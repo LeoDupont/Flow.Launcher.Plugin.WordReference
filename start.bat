cc354c4ffa147cc04c2e03ce8b978fb17b8c2d32
Garulf <535299+Garulf@users.noreply.github.com>
2022-04-22 08:28:54 -0400

    Only catch if node is missing

--- start.bat
+++ start.bat
@@ -5,9 +5,11 @@ SET icon=%plugin_dir%images\favicon-32x32.png
 SET "icon=%icon:\=/%"
 SET title="Error: Node.js is not installed on your system."
 SET subtitle="Please install node.js to use the WordReference Plugin!"
-%@Try%
-  node %plugin_dir%/src/main.js %*
-%@EndTry%
-:@Catch
-  echo {"result": [{"Title": %title%, "Subtitle": %subtitle%, "IcoPath": "%icon%"}]}
-:@EndCatch
+
+
+where /q node
+IF ERRORLEVEL 1 (
+    echo {"result": [{"Title": %title%, "Subtitle": %subtitle%, "IcoPath": "%icon%"}]}
+) ELSE (
+    node %plugin_dir%/src/main.js %*
+)
