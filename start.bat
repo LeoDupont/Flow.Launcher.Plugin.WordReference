70bd7adb27e1ede521a1942d7e0e4023d50cfa0d
Garulf <535299+Garulf@users.noreply.github.com>
2022-04-22 08:20:30 -0400

    Return result explaining error

--- start.bat
+++ start.bat
@@ -1,3 +1,13 @@
 @echo off
 SET plugin_dir=%~dp0%
-node %plugin_dir%/src/main.js %*
+SET icon=%plugin_dir%images\favicon-32x32.png
+:: JSON doesn't like backslashes so we convert them to forward slashes
+SET "icon=%icon:\=/%"
+SET title="Error: Node.js is not installed on your system."
+SET subtitle="Please install node.js to use the WordReference Plugin!"
+%@Try%
+  node %plugin_dir%/src/main.js %*
+%@EndTry%
+:@Catch
+  echo {"result": [{"Title": %title%, "Subtitle": %subtitle%, "IcoPath": "%icon%"}]}
+:@EndCatch
